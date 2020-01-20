e472bb09dc06acbe07fdfee9dd99bee08edbf1d3
Lucas Vale <lucasvale1998.lv@gmail.com>
2020-01-20 20:41:02 -0300

    Deletando Dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,43 +2,32 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Connection conn = null;
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
-	
-			st = conn.prepareStatement(
-					"UPDATE seller "
-					+ "SET BaseSalary = BaseSalary + ? "
-					+ "WHERE "
-					+ "(DepartmentId = ?)");
 
-			st.setDouble(1, 200.0);
-			st.setInt(2, 2);
-			
+			st = conn.prepareStatement("DELETE FROM department " + "WHERE " + "Id = ?");
+
+			st.setInt(1, 5);
+
 			int rowsAffected = st.executeUpdate();
-			
+
 			System.out.println("Done! Rows affected: " + rowsAffected);
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		} 
-		finally {
+		} catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		} finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
 	}
-
 }
